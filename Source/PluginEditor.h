9323b9f0fc8dbe63d68e1ee5ff9c941774112854
rogerhug <r-hug@gmx.ch>
2021-12-29 17:40:48 +0100

    Update PluginEditor.h

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -34,7 +34,7 @@ public:
     NewProjectAudioProcessorEditor (NewProjectAudioProcessor& p);
     ~NewProjectAudioProcessorEditor()
     {
-        parameters.detachControls();
+       // parameters.detachControls();//---------******##########  not function Vst3
      
     }
  
